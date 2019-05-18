9ebaa140b4341aa7b324bf2980dd6efd7500e5df
ANANT HARAL <anant@qwentic.com>
2019-05-18 20:39:47 +0530

    Made changes for service & search functionality.

--- main.go
+++ main.go
@@ -5,13 +5,13 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/stock/stock"
+	"github.com/stock/service"
 )
 
 func main() {
 	r := mux.NewRouter()
-	r.Path("/stock/{symbol}").Methods("GET").HandlerFunc(stock.GetStockBySymbol).Name("GetStockBySymbol")
-	r.Path("/stock/{symbol}").Queries("stock_exchange", "{stock_exchange}").Methods("GET").HandlerFunc(stock.GetStockBySymbol).Name("GetStockBySymbol")
+	r.Path("/stock/{symbol}").Methods("GET").HandlerFunc(service.GetStockBySymbol).Name("GetStockBySymbol")
+	r.Path("/stock/{symbol}").Queries("stock_exchange", "{stock_exchange}").Methods("GET").HandlerFunc(service.GetStockBySymbol).Name("GetStockBySymbol")
 	if err := http.ListenAndServe(":9001", r); err != nil {
 		log.Fatal(err)
 	}
